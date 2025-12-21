34c816bdf79ed4916ee032dbe47e9d1291056ac1
Killerherts <killerherts@gmail.com>
2025-12-21 00:45:30 -0600

    chore: remove build artifacts from version control

--- dist/backend/controllers/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './SettingsController';
-//# sourceMappingURL=index.d.ts.map
--- dist/backend/models/Player.d.ts
+++
@@ -1,7 +0,0 @@
-import { IPlayer } from '../types/player';
-import { BaseModelStatics } from './base';
-export interface IPlayerModel extends BaseModelStatics<IPlayer> {
-}
-export declare const Player: IPlayerModel;
-export default Player;
-//# sourceMappingURL=Player.d.ts.map
